918e3c3d285c20ad353422a13f4ee81fa6a14de4
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2022-11-07 23:38:00 -0500

    New Backend Function

--- netlify/functions/checkPaymentStatus.js
+++ netlify/functions/checkPaymentStatus.js
@@ -15,6 +15,9 @@ exports.handler = async (event, context) => {
     return { statusCode: 400, body: "paymentID too long" }
   }
   const apiRequest = await get(parsed.paymentID)
+  console.log(Number(apiRequest.pay_amount) <= Number(apiRequest.actually_paid))
+  console.log(Number(apiRequest.pay_amount))
+  console.log(Number(apiRequest.apiRequest.actually_paid))
   if (Number(apiRequest.pay_amount) <= Number(apiRequest.actually_paid)) {
     await setupBucket(parsed.orderInfo)
   }
