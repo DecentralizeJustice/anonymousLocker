ebbc7e62965284cdcfd4dbeef6fe1a650194459d
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2022-11-12 22:17:10 -0500

    Spell check faq

--- src/assets/faq.js
+++ src/assets/faq.js
@@ -4,6 +4,11 @@ const faq =
     [
         ['How Does This Work?', `I have a full demo of Anonymous Locker <a href="https://www.youtube.com/watch?v=Al7NJsf_fAU">here</a>.`   
         ],
+        ['How Much is This Service?',
+        `My service is 5 Dollars and 2% on top of the amazon order subtotal. For example, if you were to buy a 100 dollar (after amazon's taxes) hardware wallet from amazon.
+        , My service would cost you 7 dollars. At checkout with me you would pay 107 dollars of monero to me. I also always use the free shipping option. If you
+        need a faster method, just message me and we can work it out.`
+        ],
         ['Why Should I hide my shipping address?', 
         `Lets say you wanted to buy a hardware wallet. 
                 If you order from a retailer they will have your address and they could possibly leak that information. 
@@ -19,23 +24,34 @@ const faq =
         ],
         [
             'Terms of Service', 
-            `You will never have to enter any personal informtion to use my service. I do not track ip addresses, but the host of my site does. 
+            `You will never have to enter any personal information to use my service. I do not track ip addresses, but the host of my site does. 
             You are free to use tor, vpn, or any other ip hiding service. I will never ask for kyc information or any of the sorts.`
         ],
         [
             'Is the Code Open Source?', 'Yes, here is the <a href="https://github.com/DecentralizeJustice/anonymousLocker">github</a> repo for the code.'
         ]
     ],
+    'Order Questions': [
+        [
+            `Do you use prime shipping?`, 'No, I only use the free shipping option offered by amazon, unless you pay for the faster method.'
+        ],
+        [
+            `Do you offer refunds/returns?`, `Yes. I offer refunds if you have not been given the locker code yet.These refunds come with a 1% surcharge. If you have been given the locker code,
+             or have received the product you will receive a amazon gift card when you process the return. I can trade this amazon gift card for monero for you, for 2% fee.`
+        ]
+    ],
     'Alternative Methods': 
     [
         [
             `Couldn't I just ship my orders to a amazon locker myself?`, 
-            `People have been ordering to amazon lockers as a way to accomplish some privacy for years. This process can be highly variable due to amazons anti-fraud detection methods. Using a prepaid credit card with a fresh account, could result in triggering amazon's anti-fraud detection. I would recomend a prepaid card, residential proxy, burner phone number, burner email and browser emulator like multi-login or incognito if you do this proccess yourself. You might still trigger thier anti fraud detection though, so your milage will vary. If you have fiat, it would be much easier and safer to just convert that fiat to cash and buy an item in person.`
+            `People have been ordering to amazon lockers as a way to accomplish some privacy for years. This process can be highly variable due to amazons anti-fraud detection methods. 
+            Using a prepaid credit card with a fresh account, could result in triggering amazon's anti-fraud detection. I would recommend a prepaid card, residential proxy, burner phone number, burner email and browser emulator like multi-login or incognito if you do this process yourself.
+             You might still trigger their anti fraud detection though, so your mileage will vary. If you have fiat, it would be much easier and safer to just convert that fiat to cash and buy an item in person.`
 
         ],
         [
             `Can't I use purse.io?`,
-            `Purse.io has in the past allowed orders to amazon lockers. Thier current 
+            `Purse.io has in the past allowed orders to amazon lockers. Their current 
             <a href="https://purse.crisp.help/en/article/common-questions-about-name-your-discount-faq-19vz0j8/">faq</a>
             does not allow for the delivery to amazon lockers.`
         ]
