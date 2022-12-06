f89e4cbe24aafe020144032d16e3dc3ebb7355b0
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2022-12-06 13:13:15 -0500

    Answer Question

--- src/views/messageMeView.vue
+++ src/views/messageMeView.vue
@@ -44,29 +44,15 @@
         <div class="text-h6">Past Public Messages</div>
       </q-card-section>
       <q-separator />
-      <q-card-section class="text-left  q-pa-md">
-        <div class="text-body1 bg-grey-10 q-pa-md text-white">      
-          Question 3: I found you through the Monero subreddit on Reddit. It is interesting that you can pay for Amazon products with Monero and receive them in your Amazon locker. Do you have plans to support Amazon.co.jp for that? I would like to use this service in Japan!</div>
-          <div class="text-body1 q-pa-md">      
-          Answer: Thanks for the support! I would love to expand globally eventually but now I only support US Amazon. Given that I am based in the usa, I would have to do more research into ordering to amazon lockers in other countries. My app has chat built in once you pay for an item, I do not support session at this time.</div>
-      </q-card-section>
-      <q-card-section class="text-left  q-pa-md">
-        <div class="text-body1 bg-grey-10 q-pa-md text-white">      
-          Question 2: Ok so you have your own app for this service? Download from Google store?</div>
-          <div class="text-body1 q-pa-md">      
-          Answer: Hi. I only have a web app for my srvices. If you want to place an order you can place an order in the orders tab. Here is a demo video of how it works:
-          https://www.youtube.com/watch?v=Al7NJsf_fAU </div>
-      </q-card-section>
-      <q-card-section class="text-left  q-pa-md">
-        <div class="text-body1 bg-grey-10 q-pa-md text-white">      
-          Question 1: Hello buddy! I placed an order but couldnt send the xmr in time
-          When i put the 4 words nothings comes up, should i just redo the order?</div>
-          <div class="text-body1 q-pa-md">      
-          Answer: Hi. You should not refresh the page. It will take you to the correct page once you pay. There is a button at the bottom that says something like "Refresh Amount Sent", you can hit this button to check and once your payment is received the page will automatically take you to the correct page. You should not refresh the payment page.
-        The app does not store your shopping cart, till you pay for privacy reasons. So you must place the order again. Sorry for the inconvenience. If you have any questions, feel free to shoot me another message.</div>
+      <q-card-section class="text-left  q-pa-md" v-for="(item, index) in questions" :key="item.q">
+        <div class="text-body1 bg-grey-10 q-pa-md text-white">
+          Question {{getCorrectIndex(index)}}:    {{item.q}}   
+          </div>
+          <div class="text-body1 q-pa-md">
+            Answer: {{item.a}}      
+          </div>
       </q-card-section>
       
-
       <q-separator />
     </q-card>
     </div>
@@ -82,6 +68,32 @@ const axios = require('axios')
 const text = ref('')
 const successMessage = ref('')
 const buttonDisabled = ref(false)
+const questions = [
+{
+    q: `Hello, I live in Australia. AFAIK ProxyStore <https://dys2p.com/en/index.html> only forwards parcels to German addresses, your service only delivers to amazon lockers in the US and <https://purse.io/> does not support amazon lockers. 
+    Do you have any recommendations on similar services or privacy-respecting parcel forwarders that ship to Australia?`,
+    a: `Answer: I focused just on the United States at launch to start small, but I will consider expanding to Australia if you would like to be my first
+    international customer. If you wish to try out my services just submit an order. If I can't provide you with a good service, I will simply refund you. If you have a problem filling out the form, just place in fake info, and that we can have a private chat where we can work on your order. `
+  },
+  {
+    q: `I found you through the Monero subreddit on Reddit. It is interesting that you can pay for Amazon products with Monero and receive them in your Amazon locker. Do you have plans to support Amazon.co.jp for that? I would like to use this service in Japan!`,
+    a: `Answer: Thanks for the support! I would love to expand globally eventually but now I only support US Amazon. Given that I am based in the usa, I would have to do more research into ordering amazon lockers in other countries. My app has chat built in once you pay for an item, I do not support sessions at this time.`
+  },
+  {
+    q: `Ok so you have your own app for this service? Download from Google store?`,
+    a: `Hi. I only have a web app for my services. If you want to place an order you can place an order in the orders tab. Here is a demo video of how it works:
+          https://www.youtube.com/watch?v=Al7NJsf_fAU`
+  },
+  {
+    q: `Hello buddy! I placed an order but couldnt send the xmr in time
+          When i put the 4 words nothings comes up, should i just redo the order?`,
+    a:`Hi. You should not refresh the page. It will take you to the correct page once you pay. There is a button at the bottom that says something like "Refresh Amount Sent", you can hit this button to check and once your payment is received the page will automatically take you to the correct page. You should not refresh the payment page.
+        The app does not store your shopping cart, till you pay for privacy reasons. So you must place the order again. Sorry for the inconvenience. If you have any questions, feel free to shoot me another message.`
+  }
+]
+function getCorrectIndex(plainIndex){
+  return questions.length - plainIndex
+}
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
