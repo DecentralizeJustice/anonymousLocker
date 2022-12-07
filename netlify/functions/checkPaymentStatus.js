2085b8f933e1227ac19383d4e7a77879f064677d
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2022-12-07 13:39:56 -0500

    Edit First chat message to be specific.

--- netlify/functions/checkPaymentStatus.js
+++ netlify/functions/checkPaymentStatus.js
@@ -65,7 +65,7 @@ function processFirstMessage(orderDetails) {
   firstString = firstString.concat('Amazon Locker Name: ', orderDetails.lockerName, '<br/>')
   firstString = firstString.concat('Amazon Locker Zipcode: ', orderDetails.lockerZipcode, '<br/>')
   firstString = firstString.concat('Extra Order Notes: ', orderDetails.extraNotes, '<br/><br/>')
-  const outro = `I will place your order soon and message you the order details. If you need anything or have any questions, just shoot me a message here.`
+  const outro = `I will place your order within 12 hours. I'll message you here with your order details. If you need anything or have any questions, just shoot me a message here.`
   firstString = firstString.concat(outro)
   return firstString
 }
