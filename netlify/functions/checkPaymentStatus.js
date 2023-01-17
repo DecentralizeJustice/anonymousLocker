8a7ed06ef67d32e8956559516dbef6d1fecdf32a
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-01-17 11:20:18 -0500

    Test Push

--- netlify/functions/checkPaymentStatus.js
+++ netlify/functions/checkPaymentStatus.js
@@ -12,7 +12,7 @@ const words = fs.readFileSync(pathWordlist, 'utf8').toString().split("\n")
 exports.handler = async (event) => {
   // Only allow POST
   if (event.httpMethod !== "POST") {
-    return { statusCode: 405, body: "Method Not Allowed" }
+    return { statusCode: 405, body: "Method Not  Allowed" }
   }
   const params = event.body
   const parsed = JSON.parse(params)
