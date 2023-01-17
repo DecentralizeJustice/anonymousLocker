4c8139499181691b4a35f7889634ecb037a9621f
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-01-17 11:14:57 -0500

    Add redirect link

--- netlify/functions/checkPaymentStatus.js
+++ netlify/functions/checkPaymentStatus.js
@@ -3,6 +3,12 @@ const _sodium = require("libsodium-wrappers")
 const axios = require("axios")
 const Redis = require('ioredis')
 const redisPassword = process.env.redisPassword
+const fs = require('fs')
+const path = require("path")
+console.log(path.resolve(__dirname + "/bip39Wordlist.txt"))
+const pathWordlist = path.resolve(__dirname + "/bip39Wordlist.txt")
+console.log(fs.existsSync(pathWordlist))
+const words = fs.readFileSync(pathWordlist, 'utf8').toString().split("\n")
 exports.handler = async (event) => {
   // Only allow POST
   if (event.httpMethod !== "POST") {
@@ -18,6 +24,7 @@ exports.handler = async (event) => {
     console.log('ran')
     await setupBucket(parsed.orderInfo)
   }
+  await setupBucket(parsed.orderInfo)
 /*   const publicKeyBase64 = process.env.initialPublicKeyBase64 
   const privateKeyBase64 = process.env.initialPrivateKeyBase64
   const fixMistake = await decrypt(publicKeyBase64, privateKeyBase64, 'qjvG6jaKFRMFWhwI5s_3HwJjf4RdGU-Cbfw5mQBhYBJ9EBWdsDYHQHQXsuPgmjs4fgZ4R534JhhfOytU-NEct0Di4HZ9c3esyGsd')
@@ -36,11 +43,11 @@ async function get(paymentIDinput) {
   return response.data
 }
 async function setupBucket(orderInfo){
-  const firstMessageOrderInfo = processFirstMessage(orderInfo)
-  const firstMessage = { from: 'dgoon', message: firstMessageOrderInfo, sent: Date.now()}
   const publicKeyBase64 = process.env.initialPublicKeyBase64 
   const privateKeyBase64 = process.env.initialPrivateKeyBase64
   const numberArray = await decrypt(publicKeyBase64, privateKeyBase64, orderInfo.nowPaymentsInfo.order_id)
+  const firstMessageOrderInfo = processFirstMessage(orderInfo, numberArray)
+  const firstMessage = { from: 'dgoon', message: firstMessageOrderInfo, sent: Date.now()}
   const redis = new Redis({
     host: 'redis-12641.c278.us-east-1-4.ec2.cloud.redislabs.com',
     port: 12641,
@@ -50,7 +57,7 @@ async function setupBucket(orderInfo){
   redis.disconnect()
   return json
 }
-function processFirstMessage(orderDetails) {
+function processFirstMessage(orderDetails, numberArray) {
   let firstString = ''
   const welcomeMessage = 'Hi, <br/> Thank you for using Anon Shop!<br/> Your order Summary is:<br/><br/>'
   firstString = firstString.concat(welcomeMessage)
@@ -71,6 +78,8 @@ function processFirstMessage(orderDetails) {
   firstString = firstString.concat('Extra Order Notes: ', orderDetails.extraNotes, '<br/><br/>')
   const outro = `I will place your order within 12 hours. I'll message you here with your order details. If you need anything or have any questions, just shoot me a message here.`
   firstString = firstString.concat(outro)
+  const orderLink = `You can use this link to check on your order: ` + getCheckOrderLink(numberArray)
+  firstString = firstString.concat(orderLink)
   return firstString
 }
 async function decrypt(public64, private64, orderID){
@@ -80,4 +89,19 @@ async function decrypt(public64, private64, orderID){
   const soduimMessage = sodium.crypto_box_seal_open(ciphertext, sodium.from_base64(public64), sodium.from_base64(private64))
   const sodiumString = sodium.to_string(soduimMessage)
   return sodiumString
+}
+function getCheckOrderLink(numberArray){
+  const wordListFinal = numberArrayToWordArray(numberArray)
+  const link = 'https://anonshop.app/checkonorder#' + wordListFinal.join(',')
+  return link
+}
+function numberArrayToWordArray (numberArray) {
+  const wordArray = []
+  const splitNumberArray = numberArray.split(",")
+  const length = splitNumberArray.length
+  for (var i=0;i<length; i++) {
+    const wordToAdd = words[splitNumberArray[i]]
+    wordArray.push(wordToAdd.replace(/(\r\n|\n|\r)/gm, ""))
+  }
+  return wordArray
 }
