67d46b4a01197b81b15a53371e5f68456776e208
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-02-09 18:47:32 -0500

    new mobile data

--- src/components/orders/orderBook.vue
+++ src/components/orders/orderBook.vue
@@ -9,11 +9,11 @@
                     </p>
                 <q-card class="q-pa-md" style="width: 100%;">
                   <div class="row">
-                    <div class="col desktop-only">XMR Earned<br/></div>
+                    <div class="col">XMR Earned<br/></div>
                     <div class="col">USD Cost</div>
                     <div class="col desktop-only">XMR/USD</div>
                     <div class="col desktop-only">Description</div>
-                    <div class="col">Country</div>
+                    <div class="col desktop-only">Country</div>
                     <div class="col desktop-only">Number of Items</div>
                     <div class="col desktop-only">Delivery Method</div>
                     <div class="col">Order Name</div>
@@ -27,11 +27,11 @@
               <div  class="col-12 text-center row justify-center text-h5 q-my-md" v-for="order in orderBook" :key="order.link">
                 <q-card class="q-pt-md" style="width: 100%; background-color:#D4CCC4;">
                   <div class="row">
-                    <div class="col desktop-only">{{order.xmrAmount}}</div>
+                    <div class="col">{{order.xmrAmount}}</div>
                     <div class="col">~{{order.usdAmount}}</div>
-                    <div class="col desktop-only">~{{ratio(order.xmrAmount,order.usdAmount)}} XMR</div>
+                    <div class="col">~{{ratio(order.xmrAmount,order.usdAmount)}} XMR</div>
                     <div class="col desktop-only">{{order.description}}</div>
-                    <div class="col">{{order.country}}</div>
+                    <div class="col desktop-only">{{order.country}}</div>
                     <div class="col desktop-only">{{order.numberOfItems}}</div>
                     <div class="col desktop-only">{{order.type}}</div>
                     <div class="col">{{order.name}}</div>
