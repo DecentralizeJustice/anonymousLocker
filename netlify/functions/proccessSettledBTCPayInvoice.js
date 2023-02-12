3cb68e13bdedcaec81f0f50a8dedf4774727b3c3
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-02-12 16:50:17 -0500

    testing hooks

---
+++ netlify/functions/proccessSettledBTCPayInvoice.js
@@ -0,0 +1,11 @@
+/* const BTCpayKey = process.env.BTCpayKey
+const BTCpayStore = process.env.BTCpayStore
+const axios = require("axios") */
+
+exports.handler = async (event, context) => {
+    console.log(event)
+  return {
+    statusCode: 200,
+    body: ''
+  }
+}
