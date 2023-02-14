3c5a75afaea3a2dd7826c2db84a4ddb7b3dfb30e
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-02-14 13:28:55 -0500

    database has full info

--- netlify/functions/proccessSettledBTCPayInvoice.js
+++ netlify/functions/proccessSettledBTCPayInvoice.js
@@ -8,21 +8,59 @@ const uri = "mongodb+srv://main:" + mongoDBPassword + "@"+ mongoServerLocation +
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 // const webhookKey = process.env.BTCWebhookKey
 exports.handler = async (event) => {
-    console.log(event.body)
-    const collection = client.db("accounts").collection("accountInfo")
-    const query = { passphrase: "testfinginfjkhk" }
-    const exist = await collection.findOne(query)
-    if(exist !== null){ 
+
+    try {
+      console.log(event.body)
+      const collection = client.db("accounts").collection("accountInfo")
+      const params = event.body
+      const parsed = JSON.parse(params)
+      const numberArray = parsed.metadata.numberArray.toString()
+      const query = { passphrase: numberArray }
+      const exist = await collection.findOne(query)
+      if(exist !== null){ 
+        return {
+          statusCode: 409,
+          body: JSON.stringify({ error: 'account already exist' })
+        }
+      }
+      const orderInfo = {
+        nickName: 'tall chair',
+        itemList: parsed.metadata.itemList,
+        lockerZipcode: parsed.metadata.lockerZipcode,
+        lockerName: parsed.metadata.lockerName,
+        extraNotes: parsed.metadata.extraNotes,
+        type: parsed.metadata.type,
+        totalSentUSD: parsed.metadata.totalSentUSD,
+        taxAmountUSD: parsed.metadata.taxAmountUSD,
+        itemSubtotal: parsed.metadata.itemSubtotal,
+        bondUSD: parsed.metadata.bondUSD,
+        orderFeeUSD: parsed.metadata.orderFeeUSD,
+        extraAmountUSD: parsed.metadata.extraAmountUSD
+
+      }
+      const doc = { 
+        passphrase: numberArray, 
+        metaData: { 
+          email: null,
+          bondAmount: 1,
+          refundAddress: "testinginfo" 
+        },
+        orders: [
+          orderInfo
+        ] ,
+      }
+      await collection.insertOne(doc)
+      client.close()
+    return {
+      statusCode: 200,
+      body: ''
+    }
+    } catch (error) {
+      console.log(error)
       return {
-        statusCode: 409,
-        body: JSON.stringify({ error: 'account already exist' })
+        statusCode: 500,
+        body: ''
       }
     }
-    const doc = { passphrase: "testfinginfjkhk", data: { test: true } }
-    await collection.insertOne(doc)
-    client.close()
-  return {
-    statusCode: 200,
-    body: ''
-  }
+
 }
