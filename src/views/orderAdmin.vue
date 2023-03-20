9743f837f8c751dfabd0f29afdfab523e84cf383
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-03-20 14:15:49 -0400

    Push new message

--- src/views/orderAdmin.vue
+++ src/views/orderAdmin.vue
@@ -3,12 +3,15 @@
     style="width: 100%;"
     class="q-pa-md row items-stretch text-center align-center justify-center bg-primary"
   >
-    <div class="row justify-center col col-11 align-center items-stretch" >
-      <checkOnOrder/>
-    </div>
   </q-page>
 </template>
 
 <script setup>
-import checkOnOrder from "@/components/ordersPage/checkOnOrder.vue"
+import { onMounted } from "vue"
+const axios = require('axios')
+onMounted(async () => {
+  console.log('ran')
+  const results = await axios.post('/.netlify/functions/getAll', {  })
+  console.log(results.data)
+})
 </script>
