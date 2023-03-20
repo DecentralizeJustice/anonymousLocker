fc39122a2e4140d47bfab3144f9d0d0670873886
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-03-20 15:40:49 -0400

    new env vairbales

--- src/views/orderAdmin.vue
+++ src/views/orderAdmin.vue
@@ -3,14 +3,35 @@
     style="width: 100%;"
     class="q-pa-md row items-stretch text-center align-center justify-center bg-primary"
   >
+  <div class="col-12 col-md-12 col justify-center column">
+    <div class="column justify-center" style="">
+        <q-card class="col-12 col" style="">
+
+          <q-list v-for="order in orders" :key="order.passphrase">
+            <q-expansion-item popup default-closed :label="order.orders[0].nickName" >
+              <q-separator />
+              <q-card>
+                <q-card-section>
+                  <accountDashboard :accountInfo='order' :passphrase='order.passphrase'/>
+                </q-card-section>
+              </q-card>
+            </q-expansion-item>
+          </q-list>
+        </q-card>
+      </div>
+    </div>
   </q-page>
 </template>
+    
+
 
 <script setup>
-import { onMounted } from "vue"
+import { onMounted, ref } from "vue"
+import accountDashboard from '@/components/ordersPage/accountDashboard.vue'
 const axios = require('axios')
+const orders = ref([])
 onMounted(async () => {
   const results = await axios.post('/.netlify/functions/getAll', {  })
-  console.log(results.data)
+  orders.value = results.data
 })
 </script>
